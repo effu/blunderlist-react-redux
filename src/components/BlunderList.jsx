1b9c96f0b4bb273dee52d6a9029ef8c1248926ef
Joe <effulge1@live.com>
2017-06-28 16:28:13 -0600

    BlunderList created and wired with methods

--- src/components/BlunderList.jsx
+++ src/components/BlunderList.jsx
@@ -1,17 +1,36 @@
 /* componenets/BlunderList.jsx */
 
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import FlatButton from 'material-ui/FlatButton';
+import { List, ListItem } from 'material-ui/List';
 
 class BlunderList extends React.Component {
   state={}
 
+  updateParams = (randomString) => {
+    const path = `/lists/${randomString}`;
+    console.log(path);
+    this.props.showItemDetail();
+  }
+
   render() {
     return (
-      <div>
-        BlunderList is working.
-      </div>
+      <List>
+        <ListItem>
+          <FlatButton
+            label={<del>Item Name</del>}
+            onTouchTap={() => this.updateParams('item')}
+          />
+        </ListItem>
+      </List>
     );
   }
 }
+
+BlunderList.propTypes = {
+  showItemDetail: PropTypes.func.isRequired,
+};
 
 export default BlunderList;
