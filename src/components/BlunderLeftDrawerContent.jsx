c04647f09270fd84c03265413af3aaca852b0a66
Joe <effulge1@live.com>
2017-06-29 14:36:36 -0600

    added Dialog to left drawer

--- src/components/BlunderLeftDrawerContent.jsx
+++ src/components/BlunderLeftDrawerContent.jsx
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // non default exports in curly braces
 import { List, ListItem } from 'material-ui/List';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 import MenuSVG from 'material-ui/svg-icons/navigation/menu';
 
 class BlunderLeftDrawerContent extends Component {
-  state = {}
+  state = {
+    showCreateDialog: false,
+  }
 
   render() {
     return (
@@ -16,12 +21,38 @@ class BlunderLeftDrawerContent extends Component {
             onTouchTap={() => this.props.closeDrawer()}
             rightIcon={<MenuSVG />}
           />
-          
+          <ListItem
+            primaryText="Folders"
+            onTouchTap={() => this.setState({ showCreateDialog: true })}
+          />
         </List>
+        <Dialog
+          open={this.state.showCreateDialog}
+          title="Create a new list"
+          actions={[
+            <FlatButton
+              label="Cancel"
+              primary
+              onTouchTap={() => console.log('cancel dialog')}
+            />,
+            <FlatButton
+              label="Create"
+              keyboardFocused
+              onTouchTap={() => console.log('create a new list')}
+              primary
+            />,
+          ]}
+        >
+          <TextField
+            hintText={'Add a list...'}
+            onChange={(e) => { console.log(e.target.value); }}
+          />
+        </Dialog>
       </div>
     );
   }
 }
+// modal={false}
 
 BlunderLeftDrawerContent.propTypes = {
   closeDrawer: PropTypes.func.isRequired,
