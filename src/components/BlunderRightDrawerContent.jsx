40bebc49398026455ae0ccb8da6505a3468a132b
Joe <effulge1@live.com>
2017-06-29 15:14:14 -0600

    added Dialog to right drawer

--- src/components/BlunderRightDrawerContent.jsx
+++ src/components/BlunderRightDrawerContent.jsx
@@ -5,21 +5,76 @@ import {
   List,
   ListItem,
 } from 'material-ui/List';
+import TextField from 'material-ui/TextField';
+import Checkbox from 'material-ui/Checkbox';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 import MenuSVG from 'material-ui/svg-icons/navigation/menu';
 
+const defaultItem = {
+  id: 'blunder',
+  name: 'I forgot to pass out the roll.',
+  completed: false,
+};
 
 class BlunderRightDrawerContent extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDialog: false,
+      myItem: defaultItem,
+    };
+  }
+
 
   render() {
+    const { myItem } = this.state;
     return (
-      <List>
-        <ListItem
-          onTouchTap={() => this.props.closeDrawer()}
-          rightIcon={<MenuSVG />}
-        />
-      </List>
-
+      <div>
+        <List>
+          <ListItem
+            onTouchTap={() => this.props.closeDrawer()}
+            rightIcon={<MenuSVG />}
+          />
+          <ListItem>
+            <Checkbox
+              checked={myItem.completed}
+              onCheck={() => { console.log('item onCheck !completed'); }}
+            />
+            <TextField
+              fullWidth
+              hintText={'Rename the blunder'}
+              value={myItem.name}
+              onChange={this.onItemNameChange}
+              onBlur={this.onItemUpdate(myItem)}
+            />
+          </ListItem>
+          <ListItem
+            primartyText="Remove Blunder"
+            onTouchTap={() => this.setState({ showDialog: true })}
+          />
+        </List>
+        <Dialog
+          modal={false}
+          open={showDialog}
+          title={`"${myItem.name}" will be deleted forever!`}
+          actions={[
+            <FlatButton
+              label="Cancel"
+              primary
+              onTouchTap={() => this.setState({ showDialog: false })}
+            />,
+            <FlatButton
+              label="Delete Blunder"
+              primary
+              keyboardFocused
+              onTouchTap={() => this.onItemDelete(myItem)}
+            />,
+          ]}
+        >
+          You Will not be able to undo this action!
+        </Dialog>
+      </div>
     );
   }
 }
